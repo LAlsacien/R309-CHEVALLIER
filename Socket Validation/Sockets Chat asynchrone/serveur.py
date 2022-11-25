d333be6cb0821d0136b5f687c07920a9057b3b7b
LAlsacien <97260728+LAlsacien@users.noreply.github.com>
2022-11-25 13:22:10 +0100

    Socket asynchrone fini

--- Socket Validation/Sockets Chat asynchrone/serveur.py
+++ Socket Validation/Sockets Chat asynchrone/serveur.py
@@ -1,9 +1,29 @@
 import socket
+import threading
 
 host = "127.0.0.1"
 port = 10000
 usnameserver = "Serveur"
 data = ""
+msg = ""
+
+def envoi():
+    while True:
+        msg = input(f'\n{usnameserver} > ')
+        conn.send(msg.encode())
+        if msg == "bye" or msg == "exit":
+            break
+            b = "Le serveur a fermé la connexion."
+            conn.send(b.encode())
+            conn.close()
+
+def recu():
+    while True:
+        data = conn.recv(1024).decode()
+        print(f"\n{usname} > {data}")
+        if msg == "bye" or msg == "exit":
+            break
+            conn.close()
 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_server.bind((host, port))
@@ -13,12 +33,13 @@ while True:
     conn, address = socket_server.accept()
     usname = conn.recv(1024).decode()
     conn.send(usnameserver.encode())
-    while data!="exit" and data!="bye":
-        data = conn.recv(2048).decode()
-        print(f"\n{usname} > {data}")
-        msg = input(f'\n{usnameserver} > ')
-        conn.send(msg.encode())
-    conn.close()
+    
+    thread_envoi = threading.Thread(target = envoi)
+    thread_envoi.start()
+
+    thread_recu = threading.Thread(target = recu)
+    thread_recu.start()
+
     rep = input("Continuer ? (y/n)")
     if rep == "n":
         break
